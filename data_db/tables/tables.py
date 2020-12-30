e20b3e1f0e7310b85c5af13272a5fd589d85f1d3
oscarlr <olrodriguez01@gmail.com>
2020-12-29 23:05:55 -0500

    add

--- data_db/tables/tables.py
+++ data_db/tables/tables.py
@@ -1,24 +1,30 @@
 #!/bin/env python
 
 projects = """ CREATE TABLE IF NOT EXISTS projects ( 
-id integer PRIMARY KEY, 
-name text NOT NULL
+name text PRIMARY KEY
 ); """
 
 samples = """ CREATE TABLE IF NOT EXISTS samples (
-id integer PRIMARY KEY,
-name text NOT NULL,
-project_id integer NOT NULL
+name text text NOT NULL,
+project_name text NOT NULL,
+PRIMARY KEY (name, project_name),
+FOREIGN KEY (project_name) REFERENCES projects (name)
+); """
+
+probes = """ CREATE TABLE IF NOT EXISTS probes (
+name text PRIMARY KEY,
+loci text NOT NULL
 ); """
 
 capture = """ CREATE TABLE IF NOT EXISTS capture (
-id integer PRIMARY KEY,
-sample_id integer NOT NULL,
+data_path text PRIMARY KEY,
+sample_name integer NOT NULL,
 probes text NOT NULL,
-data_path text NOT NULL,
 date_added date NOT NULL,
-sequence_plex integer NOT NULL,
+sequencing_plex integer NOT NULL,
 number_reads integer NOT NULL,
-number_bases integer NOT NULL
+number_bases integer NOT NULL,
+FOREIGN KEY (sample_name) REFERENCES samples (name),
+FOREIGN KEY (probes) REFERENCES probes (name)
 ); """
 
